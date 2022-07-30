9db3e78b9fc7d6a012cb0515baba4bc13b40784a
pachipico <pachipico1@gmail.com>
2022-07-31 00:30:30 +0900

    what

--- src/main/java/com/backend/config/SecurityConfig.java
+++ src/main/java/com/backend/config/SecurityConfig.java
@@ -1,5 +1,11 @@
 package com.backend.config;
 
+import com.backend.security.CustomAccessDeniedHandler;
+import com.backend.security.CustomAuthenticationEntryPoint;
+import com.backend.security.JwtAuthenticationFilter;
+import com.backend.security.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,56 +19,48 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
-import com.backend.security.CustomAccessDeniedHandler;
-import com.backend.security.CustomAuthenticationEntryPoint;
-import com.backend.security.JwtAuthenticationFilter;
-import com.backend.security.jwt.JwtProvider;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	private final JwtProvider jwtProvider;
-	
-	private final CorsFilter corsFilter;
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 
-	@Override
-	protected AuthenticationManager authenticationManager() throws Exception {
-		
-		return super.authenticationManager();
-	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-		.httpBasic().disable()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and()
-		.authorizeRequests()
-//		.antMatchers("/api/login", "/api/signup").permitAll()
-//		.antMatchers("/api/**").hasRole("USER")
-//		.antMatchers(HttpMethod.GET, "/exception/**").permitAll()
-		.anyRequest().permitAll()
-		.and()
-		.exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
-		.and()
-		.exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
-		.and()
-		.addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
-		.addFilter(corsFilter);
-	}
-	
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/static/**", "/");
-	}
+    private final JwtProvider jwtProvider;
+
+    private final CorsFilter corsFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/login", "/api/signup").permitAll()
+                .antMatchers("/api/board/likedList", "/api/board/viewedList", "/api/board/commentedList").permitAll()
+                .antMatchers("/api/**").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/exception/**").permitAll()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .and()
+                .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
+                .addFilter(corsFilter);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/static/**", "/");
+    }
 }
