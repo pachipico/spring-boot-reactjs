7f4b1ffa3f2b1582032e7a2b570fc9d9c4711678
pachipico <pachipico1@gmail.com>
2022-08-19 00:56:19 +0900

    add: few frontend thing

---
+++ src/main/java/com/backend/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/home").addResourceLocations("file:/home/ubuntu/qnaprj/frontend/build").setCachePeriod(30);
+    }
+}
--- src/main/java/com/backend/user/handler/ProfileImgHandler.java
+++ src/main/java/com/backend/user/handler/ProfileImgHandler.java
@@ -2,6 +2,8 @@ package com.backend.user.handler;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -18,14 +20,14 @@ import net.coobird.thumbnailator.Thumbnails;
 public class ProfileImgHandler {
 
 
-//	private final String PROFILE_UP_DIR = "/Users/jhs/Desktop/ezenSpring/spring_boot/springBackend/frontend/public/profileImg";
-	private final String PROFILE_UP_DIR = "/home/ubuntu/qnaprj/frontend/build/profileImg";
-//	private final String UP_DIR = "/Users/jhs/Desktop/ezenSpring/spring_boot/springBackend/frontend/public/img";
-	private final String UP_DIR = "/home/ubuntu/qnaprj/frontend/build/img";
+	private final String PROFILE_UP_DIR = "/Users/jhs/Desktop/ezenSpring/spring_boot/springBackend/frontend/public/profileImg";
+//	private final String PROFILE_UP_DIR = "/home/ubuntu/qnaprj/frontend/build/profileImg";
+	private final String UP_DIR = "/Users/jhs/Desktop/ezenSpring/spring_boot/springBackend/frontend/public/img";
+//	private final String UP_DIR = "/home/ubuntu/qnaprj/frontend/build/img";
 	
 	public String uploadProfile(MultipartFile file) {
 
-		log.debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>??");
+		log.debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>upload profile");
 		File folders = new File(PROFILE_UP_DIR);
 		if(!folders.exists()) {
 			folders.mkdirs();
@@ -37,10 +39,10 @@ public class ProfileImgHandler {
 		UUID uuid = UUID.randomUUID();
 		
 		String fullFileName = "og_" + uuid.toString() +".jpeg";
-		File storeFile = new File(folders, fullFileName);
+		File storeFile = new File(folders.getAbsolutePath(), fullFileName);
 		try {
 			file.transferTo(storeFile);
-			File thumbnail = new File(folders, "th_" + uuid.toString());
+			File thumbnail = new File(folders, "th_" + uuid.toString() + ".jpeg");
 			Thumbnails.of(storeFile).size(100, 100).toFile(thumbnail);
 		} catch (IllegalStateException | IOException e) {
 			
@@ -50,7 +52,7 @@ public class ProfileImgHandler {
 	}
 	public String uploadFile(MultipartFile file) {
 		if(file == null) return null;
-		log.debug(">>>>>>>>>>>>>>>upload>>>>>>>>>>>>>??");
+		log.debug(">>>>>>>>>>>>>>>upload file>>>>>>>>>>>>>??");
 		File folders = new File(UP_DIR);
 		if(!folders.exists()) {
 			folders.mkdirs();
@@ -62,9 +64,11 @@ public class ProfileImgHandler {
 		UUID uuid = UUID.randomUUID();
 
 		String fullFileName = "og_" + uuid.toString() +".jpeg";
-		File storeFile = new File(folders, fullFileName);
+		File storeFile = new File(folders.getAbsolutePath(), fullFileName);
+
 		try {
 			file.transferTo(storeFile);
+
 		} catch (IllegalStateException | IOException e) {
 			e.printStackTrace();
 		}
